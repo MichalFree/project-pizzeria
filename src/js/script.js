51544b556ae164ab3c0d432c0923f732f1968c56
Michał Szymański <michal.szymanski1908@gmail.com>
2021-09-14 14:54:24 +0200

    Add displaying photos

--- src/js/script.js
+++ src/js/script.js
@@ -86,6 +86,7 @@
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
     }
     initAccordion() {
       const thisProduct = this;
@@ -147,26 +148,39 @@
 
         // for every option in this category
         for(let optionId in param.options) {
+
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
           console.log(optionId, option);
 
           // check if there is param with a name of paramId in formData and if it includes optionId
-          if(formData[paramId] && formData[paramId].includes(optionId)) {
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+
+          if (optionSelected) {
             // check if the option is not default
             if(!option.default == true) {
+
               // add option price to price variable
               price += option.price;
             }
           } else {
+
             // check if the option is default
-            if(option.default == true) {
+            if(option.default == true)
+
               // reduce price variable
               price -= option.price;
             }
+            const optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
+            if (optionImage) {
+              if (optionSelected) {
+                optionImage.classList.add(classNames.menuProduct.imageVisible);
+              } else {
+                optionImage.classList.remove(classNames.menuProduct.imageVisible);
+              }
+            }
           }
         }
-      }
 
       // update calculated price in the HTML
       thisProduct.priceElem.innerHTML = price;
